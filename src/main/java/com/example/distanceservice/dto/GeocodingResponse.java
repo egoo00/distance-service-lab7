42742c79e663b00f22e963bd86229d0c2bdde722
egoo00 <egooo914@gmail.com>
2025-06-16 00:54:05 +0300

    Update GeocodingResponse.java

--- src/main/java/com/example/distanceservice/dto/GeocodingResponse.java
+++ src/main/java/com/example/distanceservice/dto/GeocodingResponse.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class GeocodingResponse {
     private List<Result> results;
 
-    // Добавленный геттер
     public List<Result> getResults() {
         return results;
     }
 
-    // Сеттер (опционально)
     public void setResults(List<Result> results) {
         this.results = results;
     }

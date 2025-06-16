12df5bad912b828e815d7069eb8a21171736fa2c
egoo00 <egooo914@gmail.com>
2025-06-16 16:25:25 +0300

    Update CityService.java

--- src/main/java/com/example/distanceservice/service/CityService.java
+++ src/main/java/com/example/distanceservice/service/CityService.java
@@ -2,6 +2,8 @@ package com.example.distanceservice.service;
 
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.repository.CityRepository;
+import com.example.distanceservice.cache.SimpleCache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,24 +12,49 @@ import java.util.Optional;
 @Service
 public class CityService {
     private final CityRepository cityRepository;
+    private final SimpleCache simpleCache;
 
-    public CityService(CityRepository cityRepository) {
+    @Autowired
+    public CityService(CityRepository cityRepository, SimpleCache simpleCache) {
         this.cityRepository = cityRepository;
+        this.simpleCache = simpleCache;
     }
 
     public List<City> getAllCities() {
-        return cityRepository.findAll();
+        String cacheKey = "all_cities";
+        @SuppressWarnings("unchecked")
+        List<City> cachedCities = (List<City>) simpleCache.get(cacheKey);
+        if (cachedCities != null) {
+            return cachedCities;
+        }
+        List<City> cities = cityRepository.findAll();
+        simpleCache.put(cacheKey, cities);
+        return cities;
     }
 
     public Optional<City> getCityById(Long id) {
-        return cityRepository.findById(id);
+        String cacheKey = "city_" + id;
+        @SuppressWarnings("unchecked")
+        Optional<City> cachedCity = (Optional<City>) simpleCache.get(cacheKey);
+        if (cachedCity != null) {
+            return cachedCity;
+        }
+        Optional<City> city = cityRepository.findById(id);
+        simpleCache.put(cacheKey, city);
+        return city;
     }
 
     public City saveCity(City city) {
-        return cityRepository.save(city);
+        City savedCity = cityRepository.save(city);
+      
+        simpleCache.put("all_cities", null); 
+        simpleCache.put("city_" + savedCity.getId(), savedCity); 
+        return savedCity;
     }
 
     public void deleteCity(Long id) {
         cityRepository.deleteById(id);
+        simpleCache.put("all_cities", null); 
+        simpleCache.put("city_" + id, null);
     }
 }

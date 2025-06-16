fa166f9c103b296b0a863a469a08ee4c2f569034
egoo00 <egooo914@gmail.com>
2025-06-16 16:14:50 +0300

    Update CountryCityController.java

--- src/main/java/com/example/distanceservice/controller/CountryCityController.java
+++ src/main/java/com/example/distanceservice/controller/CountryCityController.java
@@ -1,8 +1,7 @@
 package com.example.distanceservice.controller;
 
 import com.example.distanceservice.entity.City;
-import com.example.distanceservice.repository.CityRepository;
-import com.example.distanceservice.cache.SimpleCache;
+import com.example.distanceservice.service.CountryCityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,39 +11,20 @@ import java.util.List;
 
 @RestController
 public class CountryCityController {
-
-    private final CityRepository cityRepository;
-    private final SimpleCache simpleCache;
+    private final CountryCityService countryCityService;
 
     @Autowired
-    public CountryCityController(CityRepository cityRepository, SimpleCache simpleCache) {
-        this.cityRepository = cityRepository;
-        this.simpleCache = simpleCache;
+    public CountryCityController(CountryCityService countryCityService) {
+        this.countryCityService = countryCityService;
     }
 
     @GetMapping("/api/cities-by-country")
     public List<City> getCitiesByCountry(@RequestParam String countryName) {
-        String cacheKey = "cities_" + countryName;
-        Object cached = simpleCache.get(cacheKey);
-        if (cached instanceof List<?> cachedCities) {
-            return (List<City>) cachedCities;
-        }
-
-        List<City> cities = cityRepository.findByCountryName(countryName);
-        simpleCache.put(cacheKey, cities);
-        return cities;
+        return countryCityService.getCitiesByCountry(countryName);
     }
 
     @GetMapping("/api/cities-by-country-native")
     public List<City> getCitiesByCountryNative(@RequestParam String countryName) {
-        String cacheKey = "cities_native_" + countryName;
-        Object cached = simpleCache.get(cacheKey);
-        if (cached instanceof List<?> cachedCities) {
-            return (List<City>) cachedCities;
-        }
-
-        List<City> cities = cityRepository.findByCountryNameNative(countryName);
-        simpleCache.put(cacheKey, cities);
-        return cities;
+        return countryCityService.getCitiesByCountryNative(countryName);
     }
 }

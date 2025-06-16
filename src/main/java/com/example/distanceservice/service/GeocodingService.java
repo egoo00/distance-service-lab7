3f5a54ab9027d013d30311dc77077543f45c4385
egoo00 <egooo914@gmail.com>
2025-06-16 16:29:24 +0300

    Update GeocodingService.java

--- src/main/java/com/example/distanceservice/service/GeocodingService.java
+++ src/main/java/com/example/distanceservice/service/GeocodingService.java
@@ -1,16 +1,34 @@
 package com.example.distanceservice.service;
 
 import com.example.distanceservice.entity.City;
+import com.example.distanceservice.cache.SimpleCache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeocodingService {
+    private final SimpleCache simpleCache;
+
+    @Autowired
+    public GeocodingService(SimpleCache simpleCache) {
+        this.simpleCache = simpleCache;
+    }
+
     public City getCity(String cityName) {
-        if ("Минск".equalsIgnoreCase(cityName)) {
-            return new City(cityName, 53.9, 27.5667);
-        } else if ("Варшава".equalsIgnoreCase(cityName)) {
-            return new City(cityName, 52.2297, 21.0122);
+        String cacheKey = "geocode_" + cityName.toLowerCase();
+        @SuppressWarnings("unchecked")
+        City cachedCity = (City) simpleCache.get(cacheKey);
+        if (cachedCity != null) {
+            return cachedCity;
         }
-        return null;
+
+        City city = null;
+        if ("Минск".equalsIgnoreCase(cityName)) {
+            city = new City(cityName, 53.9, 27.5667);
+        } else if ("Варшава".equalsIgnoreCase(cityName)) {
+            city = new City(cityName, 52.2297, 21.0122);
+        }
+        simpleCache.put(cacheKey, city);
+        return city;
     }
 }

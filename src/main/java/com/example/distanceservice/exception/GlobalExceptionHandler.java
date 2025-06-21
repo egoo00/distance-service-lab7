78296adc703d0696cda6ef09ab14a16968d6135a
egoo00 <egooo914@gmail.com>
2025-06-21 08:45:46 +0300

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/distanceservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/distanceservice/exception/GlobalExceptionHandler.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final HttpStatus BAD_REQUEST_STATUS = HttpStatus.BAD_REQUEST;
+    private static final HttpStatus INTERNAL_SERVER_ERROR_STATUS = HttpStatus.INTERNAL_SERVER_ERROR;
+    private static final String UNEXPECTED_ERROR_MESSAGE = "An unexpected error occurred: ";
+
     @ExceptionHandler(CityNotFoundException.class)
     public ResponseEntity<String> handleCityNotFoundException(CityNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ex.getMessage(), BAD_REQUEST_STATUS);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(UNEXPECTED_ERROR_MESSAGE + ex.getMessage(), INTERNAL_SERVER_ERROR_STATUS);
     }
 }

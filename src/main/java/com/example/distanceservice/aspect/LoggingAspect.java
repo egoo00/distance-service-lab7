8f5fc00de3d9008ae6b123c2abb7db0c1c188bd4
egoo00 <egooo914@gmail.com>
2025-06-21 07:57:36 +0300

    Update LoggingAspect.java

--- src/main/java/com/example/distanceservice/aspect/LoggingAspect.java
+++ src/main/java/com/example/distanceservice/aspect/LoggingAspect.java
@@ -1,7 +1,10 @@
 package com.example.distanceservice.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -9,20 +12,21 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+    private static final String CONTROLLER_PACKAGE = "com.example.distanceservice.controller.*.*(..)";
 
-    @Before("execution(* com.example.distanceservice.controller.*.*(..))")
+    @Before("execution(* " + CONTROLLER_PACKAGE + ")")
     public void logBefore(JoinPoint joinPoint) {
-        logger.info("Entering method: " + joinPoint.getSignature().getName());
+        LOGGER.info("Entering method: " + joinPoint.getSignature().getName());
     }
 
-    @AfterReturning(pointcut = "execution(* com.example.distanceservice.controller.*.*(..))", returning = "result")
+    @AfterReturning(pointcut = "execution(* " + CONTROLLER_PACKAGE + ")", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        logger.info("Method " + joinPoint.getSignature().getName() + " returned: " + result);
+        LOGGER.info("Method " + joinPoint.getSignature().getName() + " returned: " + result);
     }
 
-    @AfterThrowing(pointcut = "execution(* com.example.distanceservice.controller.*.*(..))", throwing = "exception")
+    @AfterThrowing(pointcut = "execution(* " + CONTROLLER_PACKAGE + ")", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        logger.error("Exception in method " + joinPoint.getSignature().getName() + ": " + exception.getMessage(), exception);
+        LOGGER.error("Exception in method " + joinPoint.getSignature().getName() + ": " + exception.getMessage(), exception);
     }
 }

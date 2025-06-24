7524e6f4266955dee0d99c0c90cb19b06d58ebc1
egoo00 <egooo914@gmail.com>
2025-06-24 17:50:33 +0300

    Update CountryCityController.java

--- src/main/java/com/example/distanceservice/controller/CountryCityController.java
+++ src/main/java/com/example/distanceservice/controller/CountryCityController.java
@@ -2,7 +2,6 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.service.CountryCityService;
-import com.example.distanceservice.util.RequestCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,41 +11,20 @@ import java.util.List;
 
 @RestController
 public class CountryCityController {
-    private static final String API_PATH_CITIES_BY_COUNTRY = "/api/cities-by-country";
-    private static final String API_PATH_CITIES_BY_COUNTRY_NATIVE = "/api/cities-by-country-native";
-
     private final CountryCityService countryCityService;
-    private final RequestCounter requestCounter;
 
     @Autowired
-    public CountryCityController(CountryCityService countryCityService, RequestCounter requestCounter) {
+    public CountryCityController(CountryCityService countryCityService) {
         this.countryCityService = countryCityService;
-        this.requestCounter = requestCounter;
     }
 
-    @GetMapping(API_PATH_CITIES_BY_COUNTRY)
+    @GetMapping("/api/cities-by-country")
     public List<City> getCitiesByCountry(@RequestParam String countryName) {
-        requestCounter.incrementTotal();
-        try {
-            List<City> cities = countryCityService.getCitiesByCountry(countryName);
-            requestCounter.incrementSuccessful();
-            return cities;
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+        return countryCityService.getCitiesByCountry(countryName);
     }
 
-    @GetMapping(API_PATH_CITIES_BY_COUNTRY_NATIVE)
+    @GetMapping("/api/cities-by-country-native")
     public List<City> getCitiesByCountryNative(@RequestParam String countryName) {
-        requestCounter.incrementTotal();
-        try {
-            List<City> cities = countryCityService.getCitiesByCountryNative(countryName);
-            requestCounter.incrementSuccessful();
-            return cities;
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+        return countryCityService.getCitiesByCountryNative(countryName);
     }
 }

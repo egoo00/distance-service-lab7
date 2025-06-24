2f6a37e93148a07ae3f94b8e3161305818d001bf
egoo00 <egooo914@gmail.com>
2025-06-24 17:50:53 +0300

    Update CountryController.java

--- src/main/java/com/example/distanceservice/controller/CountryController.java
+++ src/main/java/com/example/distanceservice/controller/CountryController.java
@@ -1,76 +1,39 @@
 package com.example.distanceservice.controller;
 
-import com.example.distanceservice.entity.Country;
-import com.example.distanceservice.service.CountryService;
+import com.example.distanceservice.dto.DistanceResponse;
+import com.example.distanceservice.service.DistanceService;
 import com.example.distanceservice.util.RequestCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/countries")
-public class CountryController {
-    private static final String API_PATH = "/api/countries";
+@RequestMapping("/api")
+public class DistanceController {
 
-    private final CountryService countryService;
+    private final DistanceService distanceService;
     private final RequestCounter requestCounter;
 
     @Autowired
-    public CountryController(CountryService countryService, RequestCounter requestCounter) {
-        this.countryService = countryService;
+    public DistanceController(DistanceService distanceService, RequestCounter requestCounter) {
+        this.distanceService = distanceService;
         this.requestCounter = requestCounter;
     }
 
-    @GetMapping
-    public List<Country> getAllCountries() {
-        requestCounter.incrementTotal();
-        try {
-            List<Country> countries = countryService.getAllCountries();
-            requestCounter.incrementSuccessful();
-            return countries;
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+    @GetMapping("/distance")
+    public DistanceResponse getDistance(@RequestParam String from, @RequestParam String to) {
+        return distanceService.calculateDistance(from, to);
     }
 
-    @GetMapping("/{id}")
-    public Optional<Country> getCountryById(@PathVariable Long id) {
-        requestCounter.incrementTotal();
-        try {
-            Optional<Country> country = countryService.getCountryById(id);
-            requestCounter.incrementSuccessful();
-            return country;
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+    @PostMapping("/distances/bulk")
+    public List<DistanceResponse> getBulkDistances(@RequestBody List<String[]> cityPairs) {
+        return distanceService.calculateBulkDistances(cityPairs);
     }
 
-    @PostMapping
-    public Country saveCountry(@RequestBody Country country) {
-        requestCounter.incrementTotal();
-        try {
-            Country savedCountry = countryService.saveCountry(country);
-            requestCounter.incrementSuccessful();
-            return savedCountry;
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteCountry(@PathVariable Long id) {
-        requestCounter.incrementTotal();
-        try {
-            countryService.deleteCountry(id);
-            requestCounter.incrementSuccessful();
-        } catch (Exception e) {
-            requestCounter.incrementFailed();
-            throw e;
-        }
+    @GetMapping("/counter")
+    public int getRequestCount() {
+        return requestCounter.getCount();
     }
 }

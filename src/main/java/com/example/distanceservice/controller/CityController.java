c0c73956495dbed81fd640bec1d6e50551bea9b9
egoo00 <egooo914@gmail.com>
2025-06-24 17:50:15 +0300

    Update CityController.java

--- src/main/java/com/example/distanceservice/controller/CityController.java
+++ src/main/java/com/example/distanceservice/controller/CityController.java
@@ -2,7 +2,6 @@ package com.example.distanceservice.controller;
 
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.service.CityService;
-import com.example.distanceservice.util.RequestCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,61 +14,44 @@ public class CityController {
     private static final String API_PATH = "/api/cities";
 
     private final CityService cityService;
-    private final RequestCounter requestCounter;
 
     @Autowired
-    public CityController(CityService cityService, RequestCounter requestCounter) {
+    public CityController(CityService cityService) {
         this.cityService = cityService;
-        this.requestCounter = requestCounter;
     }
 
     @GetMapping
     public List<City> getAllCities() {
-        requestCounter.incrementTotal();
         try {
-            List<City> cities = cityService.getAllCities();
-            requestCounter.incrementSuccessful();
-            return cities;
+            return cityService.getAllCities();
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @GetMapping("/{id}")
     public Optional<City> getCityById(@PathVariable Long id) {
-        requestCounter.incrementTotal();
         try {
-            Optional<City> city = cityService.getCityById(id);
-            requestCounter.incrementSuccessful();
-            return city;
+            return cityService.getCityById(id);
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @PostMapping
     public City saveCity(@RequestBody City city) {
-        requestCounter.incrementTotal();
         try {
-            City savedCity = cityService.saveCity(city);
-            requestCounter.incrementSuccessful();
-            return savedCity;
+            return cityService.saveCity(city);
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
 
     @DeleteMapping("/{id}")
     public void deleteCity(@PathVariable Long id) {
-        requestCounter.incrementTotal();
         try {
             cityService.deleteCity(id);
-            requestCounter.incrementSuccessful();
         } catch (Exception e) {
-            requestCounter.incrementFailed();
             throw e;
         }
     }
